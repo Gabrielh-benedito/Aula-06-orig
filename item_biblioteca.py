456831d1a089ef61e54830e9d77ffe88a7c0bb26
Gabriel Benedito <Gabrielh.beneditoo@gmail.com>
2026-03-04 10:13:13 -0300

    Função set titulo e set ano ✔

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -10,3 +10,19 @@ class ItemBiblioteca:
     
     def get_titulo(self):
         return self.__titulo
+    
+    def get_ano(self):
+        return self.__ano
+
+    def get_disponivel(self):
+        return self.__disponivel
+    
+    def set_titulo(self, titulo):
+        if titulo.strip() == "":
+            raise ValueError("Título não pode ser vazio.")
+        self.__titulo = titulo
+
+    def set_ano(self, ano):
+        if ano <= 0:
+            raise ValueError("Ano deve ser maior que 0.")
+        self.__ano = ano
