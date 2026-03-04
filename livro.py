5a1d8f1af2dcee38d9e5ff816d6a61eb16e7f5a6
Gabriel Benedito <Gabrielh.beneditoo@gmail.com>
2026-03-04 10:18:27 -0300

    Função exibir detalhes ✔✔✔

--- livro.py
+++ livro.py
@@ -6,3 +6,11 @@ class Livro(ItemBiblioteca):
         self.__autor = autor
         self.__num_paginas = num_paginas
 
+    def exibir_detalhes(self):
+        status = "Disponível" if self.get_disponivel() else "Emprestado"
+        print(f"[Livro] Código: {self.get_codigo()} | "
+              f"Título: {self.get_titulo()} | "
+              f"Autor: {self.__autor} | "
+              f"Páginas: {self.__num_paginas} | "
+              f"Ano: {self.get_ano()} | "
+              f"Status: {status}")
