6af99fe02569e65ff6000f770d039789f70465d6
Gabriel Benedito <Gabrielh.beneditoo@gmail.com>
2026-03-04 08:58:20 -0300

    Função adicionar item ✔

--- biblioteca.py
+++ biblioteca.py
@@ -1,1 +1,7 @@
-#a
+class biblioteca:
+    def __init__(self):
+        self._itens = []
+
+    def adicionar_item(self, item):
+        self._itens.append(item)
+        print(f"Item adicionado com sucesso")
