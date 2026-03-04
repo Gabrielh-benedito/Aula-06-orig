7457d457df42a909c803cc2aad03259c829f5dea
Gabriel Benedito <Gabrielh.beneditoo@gmail.com>
2026-03-04 10:25:37 -0300

    MAIN (Menu completo) ✔

--- Revista.py
+++ Revista.py
@@ -5,4 +5,12 @@ class Revista(ItemBiblioteca):
         super().__init__(codigo, titulo, ano)
         self.__edicao = edicao
         self.__mes = mes
-
+   
+    def exibir_detalhes(self):
+        status = "Disponível" if self.get_disponivel() else "Emprestado"
+        print(f"[Revista] Código: {self.get_codigo()} | "
+              f"Título: {self.get_titulo()} | "
+              f"Edição: {self.__edicao} | "
+              f"Mês: {self.__mes} | "
+              f"Ano: {self.get_ano()} | "
+              f"Status: {status}")
